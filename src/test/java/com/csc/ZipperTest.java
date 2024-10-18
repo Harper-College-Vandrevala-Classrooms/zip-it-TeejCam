371e9ea0c163bd3a8c7125418e7f43d9e0b440d1
TeejCam <tjcamalan@gmail.com>
2024-10-17 20:00:07 -0500

    Added the zip function and added test cases to the test file

--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -1,4 +1,6 @@
 package com.csc;
+import java.util.List;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,8 +16,37 @@ public class ZipperTest {
     zipper = new Zipper();
   }
 
-  @Test
+  /**@Test
   void itWorks() {
     assertEquals(true, true);
+  }*/
+
+  @Test 
+  public void testListSize() {
+    List<Integer> list1 = List.of(1, 2, 3, 4);
+    List<Integer> list2 = List.of(2, 3, 5, 6);
+    assertEquals(4, zipper.listSize(list1, list2));
   }
+
+  @Test
+  public void testStringListSize() {
+    List<String> list1 = List.of("Green", "Blue");
+    List<String> list2 = List.of("Black", "White");
+    assertEquals(2, zipper.listSize(list1, list2));
+  }
+
+  @Test 
+  public void testListOneBigger() {
+    List<Integer> list1 = List.of(1, 2, 3, 4);
+    List<Integer> list2 = List.of(1, 2, 3);
+    assertEquals(3, zipper.listSize(list1, list2));
+  }
+
+  @Test
+  public void testListTwoBigger() {
+    List<Character> list1 = List.of('a', 'b', 'c');
+    List<Character> list2 = List.of('a', 'b', 'c', 'd');
+    assertEquals(3, zipper.listSize(list1, list2));
+  }
+
 }
